dae64172780a42d525092e5816ac6de4fd363ae3
zhouchongqing <1021389278@qq.com>
2022-12-27 23:52:23 +0800

    java 写的音乐播放器

---
+++ MyMusicplayer/app/src/main/java/com/example/myzuoye/frag1.java
@@ -0,0 +1,57 @@
+package com.example.myzuoye;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+public class frag1 extends Fragment {
+    private View view;
+    public static String[] name={"百里屠扶——在意我","程响——等你归来","王空阳——空","周杰伦——简单爱"};
+    public static int[] icons={R.drawable.music0,R.drawable.music1,R.drawable.music2,R.drawable.music3};
+    @Override
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        view=inflater.inflate(R.layout.music_list,null);
+        ListView listView=view.findViewById(R.id.lv);
+        MyBaseAdapter adapter=new MyBaseAdapter();
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent=new Intent(frag1.this.getContext(),Music_Activity.class);//创建Intent对象，启动check
+                //将数据存入Intent对象
+                intent.putExtra("name",name[position]);
+                intent.putExtra("position",String.valueOf(position));
+                startActivity(intent);
+            }
+        });
+        return view;
+    }
+    class MyBaseAdapter extends BaseAdapter{
+        @Override
+        public int getCount(){return  name.length;}
+        @Override
+        public Object getItem(int i){return name[i];}
+        @Override
+        public long getItemId(int i){return i;}
+
+        @Override
+        public View getView(int i ,View convertView, ViewGroup parent) {
+            View view=View.inflate(frag1.this.getContext(),R.layout.item_layout,null);
+            TextView tv_name=view.findViewById(R.id.item_name);
+            ImageView iv=view.findViewById(R.id.iv);
+            tv_name.setText(name[i]);
+            iv.setImageResource(icons[i]);
+            return view;
+        }
+    }
+
+
+}
